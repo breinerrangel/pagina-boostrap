3a336ed1ae1aa60d719697906935e639616fd9f8
neferyesid <neferysg@gmail.com>
2023-02-15 12:50:27 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let compra1=document.getElementById("1")
+let compra2=document.getElementById("2")
+let compra3=document.getElementById("3")
+let compra4=document.getElementById("4")
+let compra5=document.getElementById("5")
+let compra6=document.getElementById("6")
+
+
+
+compra1.addEventListener("click",function(){
+    alert("GRACIAS POR COMPRAR AQUI")
+}
+)
+compra2.addEventListener("click",function(){
+    alert("GRACIAS POR COMPRAR AQUI")
+}
+)
+compra3.addEventListener("click",function(){
+    alert("GRACIAS POR COMPRAR AQUI")
+}
+)
+compra4.addEventListener("click",function(){
+    alert("GRACIAS POR COMPRAR AQUI")
+}
+)
+compra5.addEventListener("click",function(){
+    alert("GRACIAS POR COMPRAR AQUI")
+}
+)
+compra6.addEventListener("click",function(){
+    alert("GRACIAS POR COMPRAR AQUI")
+}
+)
+
+
+    
+
